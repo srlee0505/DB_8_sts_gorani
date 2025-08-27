1721da970222fc4aba1e9dd9a57a8183fd891315
srlee <sr120505@naver.com>
2025-08-27 10:48:41 +0900

    commit

---
+++ src/main/java/com/example/demo/Db8StsGoraniApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db8StsGoraniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Db8StsGoraniApplication.class, args);
+	}
+
+}
